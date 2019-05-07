01ec43837d36a9f32555da60ac5b7f148ce4f67f
Raymond Liang <raymondliang@u-10-81-44-51.dhcp.unlv.edu>
2019-05-06 17:37:54 -0700

    1

--- js/app.js
+++ js/app.js
@@ -19,6 +19,16 @@ function handleError(error) {
 
 var callerName = "";
 var publisherName = "";
+var currentdate = new Date();
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+console.log(datetime);
+
 function initializeSession() {
 
   var session = OT.initSession(apiKey, sessionId);
